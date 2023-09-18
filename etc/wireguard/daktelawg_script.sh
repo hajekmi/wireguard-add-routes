8137a06e7539dd1f40f647aee21f08d7365322f4
root <root@zuzka.online>
2023-09-18 09:45:50 +0200

    add some checks

--- etc/wireguard/daktelawg_script.sh
+++ etc/wireguard/daktelawg_script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
 action=$1
 wg_interface=$2
 wg_server=$3
 url_get_routes=$4
 metric=8;
+
+
 
 add_route() {
     local net=$1
@@ -42,16 +45,20 @@ download_routes() {
                 if [ -n "${ipmask}" ]; then
                     echo "run func add_route ${ipmask} \"\" ${wg_interface} ${metric}"
                     add_route ${ipmask} "" ${wg_interface} ${metric}
+                    if [ $? -ge 1 ]; then
+                        return 2;
+                    fi
                 fi
             done < ${tmp}
 
+            echo "Success added all routes"
             rm -f "${tmp}"
             return 0;
         fi
     done
     
     rm -f "${tmp}"
-    return 0;
+    return 1;
 }
 
 wg_server_default_route() {
@@ -59,9 +66,11 @@ wg_server_default_route() {
         local default_gw=$( ip r | grep default | sed 's~.* via \([0-9.]*\) .*~\1~' 2> /dev/null )
         local default_dev=$( ip r | grep default | sed 's~.* dev \([^ ]*\) .*~\1~' 2> /dev/null )
         ip r del ${wg_server} metric $((${metric}-1)) > /dev/null 2>&1
-        add_route "${wg_server}/32" "${default_gw}" "${default_dev}" "$((${metric}-1))" || exit 1;
+        add_route "${wg_server}/32" "${default_gw}" "${default_dev}" "$((${metric}-1))";
+        return $?
     else
         ip r del ${wg_server} metric $((${metric}-1))
+        return 0;
     fi
     return;
 }
@@ -72,18 +81,17 @@ run_up() {
     add_route "100.64.0.0/16" "" "${wg_interface}" "${metric}" || exit 1;
 
     # Route for WG server public
-    wg_server_default_route 1
+    wg_server_default_route 1 || exit 2;
 
     # Download all routes
-    download_routes
+    download_routes || exit 3;
 
     ## Custom add routes here:
-    
 
     ## end custom
   
     # Firewall
-    # /michals/fw_rules.sh
+    #/michals/fw_rules.sh
 
     return;
 }
